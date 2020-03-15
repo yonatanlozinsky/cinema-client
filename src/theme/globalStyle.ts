a18fea98649aabb06b5fbc0fbc076e3d8cfe7b61
Yonatan Lozinsky <igonnabanu@gmail.com>
2020-03-15 20:42:56 +0200

    remove global style issue

--- src/theme/globalStyle.ts
+++ src/theme/globalStyle.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components/macro';
 
 export default createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css?family=Fredoka+One&display=swap');
     body {
         font-family: Roboto, sans-serif;
         color: white;
